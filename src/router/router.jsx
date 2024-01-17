f5b94bc686c1c71a003ffd1469e6d48745ead8d1
shaikot07 <saifulislamshaikat007@gmail.com>
2024-01-17 23:54:57 +0600

    create the login page

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,16 +3,31 @@ import { createBrowserRouter } from "react-router-dom";
 
 // main layout
 import RootLayout from "../components/layouts/RootLayout";
-
+import AuthLayout from "./../components/layouts/AuthLayout"
 // page components
 import Home from "../components/pages/Home/Home";
+import LoginPage from "../components/pages/LoginPage/LoginPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
-    children: [{ path: "/", element: <Home /> }],
+    children: [{ path: "/",
+    element: <Home /> },
+  ],
   },
+  {
+    path: "/login",
+    element: <AuthLayout/>,
+    children: [
+    // login route 
+    {
+      path:"/login",
+      element:<LoginPage/>
+    },
+  ],
+  },
+
 ]);
 
 export default router;
