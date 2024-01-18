a33b2e773d41066d0861820a716e3a4c4c2c834c
shaikot07 <saifulislamshaikat007@gmail.com>
2024-01-18 21:25:47 +0600

    updated deals of the week section

--- src/components/pages/Home/DealsOfTheWeekCarousel/DealsOfTheWeekCarousel.jsx
+++ src/components/pages/Home/DealsOfTheWeekCarousel/DealsOfTheWeekCarousel.jsx
@@ -40,26 +40,26 @@ const DealsOfTheWeekCarousel = () => {
                               {
                                     books.map((book) => (
                                           <SwiperSlide key={book.id}>
-                                                <div className='bg-[#FFFFFF]  flex gap-3  items-center border p-6'>
-                                                      <div className='w-[180px] h-[190px] border'>
+                                                <div className='bg-[#FFFFFF] md:flex gap-3   items-center border p-6'>
+                                                      <div className='w-[180px] h-[190px] border overflow-hidden'>
                                                             <img className='object-cover' src={book.image} alt={book.bookName} />
                                                       </div>
-                                                      <div className=' w-full'>
-                                                            <div className='flex  justify-between '>
+                                                      <div className=' w-full '>
+                                                            <div className='md:flex  justify-between '>
                                                                   <div className=''>
                                                                         <p className='text-[#F85455]'>Publisher</p>
                                                                         <p className=' text-sm text-[#373739]'>{book.publisher}</p>
                                                                         <p className=' text-sm text-[#373739]'>Author: {book.author}</p>
                                                                         <p className=' text-base font-bold text-[#1E1E1E]'>$ {book.price}</p>
                                                                         <p className=' text-yellow-300 flex'><IoMdStar /> <IoMdStar /> <IoMdStar /> <IoMdStarHalf /> </p>
-                                                                        <p><span className='text-1xl text-[#1E1E1E] font-bold'>Hurry Up!</span> Offer ends soon</p>
+                                                                        <p><span className='text-1xl text-[#1E1E1E] font-bold'>Hurry Up!</span> Offer ends in</p>
                                                                   </div>
                                                                   <div className='w-[70px] h-[70px] text-center font-semibold p-2 rounded-full bg-[#F85455]'>
                                                                         <p className='text-white'><span>Save</span><br />$ 50</p>
                                                                   </div>
                                                             </div>
-                                                            <div className='grid grid-cols-4 divide-x gap-2  items-center justify-center '>
-                                                                  <div className='bg-[#FFFFFF] p-2'><h1 className='text-base text-center font-semibold'>114 <span className='text-sm text-[#373739]'>Days</span></h1></div>
+                                                            <div className='grid grid-cols-2 md:grid-cols-4 divide-x gap-2  items-center justify-center '>
+                                                                  <div className='bg-[#FFFFFF] py-2'><h1 className='text-base text-left font-semibold'>114 <span className='text-sm text-[#373739]'>Days</span></h1></div>
                                                                   <div className='bg-[#FFFFFF] p-2'><h1 className='text-base ml-2 text-center font-semibold'>04<span className='text-sm text-[#373739]'>Hours</span></h1></div>
                                                                   <div className='bg-[#FFFFFF] p-2'><h1 className='text-base ml-2 text-center font-semibold'>60 <span className='text-sm text-[#373739]'>Mins</span></h1></div>
                                                                   <div className='bg-[#FFFFFF] p-2'><h1 className='text-base ml-2 text-center font-semibold'>25 <span className='text-sm text-[#373739]'>Secs</span></h1></div>
