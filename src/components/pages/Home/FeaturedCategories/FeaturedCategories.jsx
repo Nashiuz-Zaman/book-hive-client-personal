1180bc61c418d8caec4d7a93ade343a882391424
Shazzad-Shuvo <md.rahman.shazzadur@gmail.com>
2024-01-19 12:23:31 +0600

    featured categories and bestselling books section added and made responsive

--- src/components/pages/Home/FeaturedCategories/FeaturedCategories.jsx
+++ src/components/pages/Home/FeaturedCategories/FeaturedCategories.jsx
@@ -1,14 +1,29 @@
-import InnerContainer from "../../../containers/InnerContainer/InnerContainer";
-import { IoMdImages } from "react-icons/io";
-import { BiDish } from "react-icons/bi";
+// raw data
+import { featuredCategories } from '../../../../../public/featuredCategories';
+
+// react icons
+import { FaAnglesRight } from 'react-icons/fa6';
+
+// component
+import CategoryCard from './CategoryCard';
+
 
 const FeaturedCategories = () => {
+    console.log(featuredCategories);
     return (
         <section className="mt-20">
-            <InnerContainer>
-                <div className="grid grid-cols-5 gap-8">
-                    {/* feature card-1 */}
-                    <div className="p-12 bg-purple-100">
+            <div className='flex justify-between mb-10'>
+                <h2 className='text-2xl font-semibold'>Featured Categories</h2>
+                <div>
+                    <button className="text-base  hover:bg-[#F85455] hover:text-white py-2 px-3 hover:rounded-3xl">
+                        View All
+                        <FaAnglesRight className="inline-block ml-2" />
+                    </button>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
+                {/* feature card-1 */}
+                {/* <div className="p-12 bg-purple-100">
                         <div>
                             <IoMdImages size={60} className="text-purple-500 transform -scale-x-100"></IoMdImages>
                         </div>
@@ -18,10 +33,17 @@ const FeaturedCategories = () => {
                                 <button className="hover:underline">Shop Now</button>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </InnerContainer>
+                    </div> */}
+                {
+                    featuredCategories.map((card, index) =>
+                        <CategoryCard
+                            key={index}
+                            card={card}
+                        />
 
+                    )
+                }
+            </div>
         </section>
     );
 };
