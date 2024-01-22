29b0d6678829acf933333a58aec3b0eb975a84a1
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-01-22 20:12:39 +0600

    Set up login and registration forms in both pages, also correct some bugs in router

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -7,13 +7,15 @@ import AuthLayout from "./../components/layouts/AuthLayout";
 // page components
 import Home from "../components/pages/Home/Home";
 import LoginPage from "../components/pages/LoginPage/LoginPage";
-import RegistrationPage from "../components/pages/Registration/RegistrationPage";
+import ErrorPage from "./../components/pages/ErrorPage/ErrorPage";
+import RegistrationPage from "./../components/pages/RegistrationPage/RegistrationPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [{ path: "/", element: <Home /> }],
+    errorElement: <ErrorPage />,
   },
   {
     path: "/login",
@@ -27,12 +29,12 @@ const router = createBrowserRouter([
     ],
   },
   {
-    path: "/register",
+    path: "/registration",
     element: <AuthLayout />,
     children: [
       // registration route
       {
-        path: "/register",
+        path: "/registration",
         element: <RegistrationPage />,
       },
     ],
