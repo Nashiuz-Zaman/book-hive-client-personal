1f96bc600bd1def7b8cb4f6d58daa046655361e4
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-01-26 20:02:11 +0600

    Refactor code

--- src/components/pages/Home/PopularAuthors/PopularAuthers.jsx
+++ src/components/pages/Home/PopularAuthors/PopularAuthers.jsx
@@ -5,7 +5,7 @@ import PopularAutherCard from "./PopularAutherCard";
 
 const PopularAuthers = () => {
   return (
-    <div className="mx-auto bg-[#fff6f7] px-3 py-20">
+    <div className="mx-auto px-3 py-20">
       <h2 className="font-semibold text-2xl mb-10 ">Popular Authors</h2>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8 font-medium">
         {popularAuthers.map((auther, i) => (
