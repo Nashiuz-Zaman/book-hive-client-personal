6e4c39712229d94d96c7ebb0c77bca8286de42f7
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-01-26 11:34:25 +0600

    Update navigation options data

--- src/uiData/navigationOptions.js
+++ src/uiData/navigationOptions.js
@@ -2,4 +2,5 @@ export const navOptions = [
   { id: 0, text: "Home", url: "/" },
   { id: 1, text: "Books Collection", url: "allBooks" },
   { id: 2, text: "Shop", url: "#" },
+  { id: 3, text: "Contact", url: "#" },
 ];
