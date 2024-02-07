6d9fcc07c04f5053ad932aa0f6dba63a7315ead2
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-02-07 12:31:05 +0600

    Add a link in the website header which can navigate to the dashboard

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -14,40 +14,58 @@ import LargeScreenNav from "../LargeScreenNav/LargeScreenNav";
 import Searchbox from "../Searchbox/Searchbox";
 
 // hooks
-// import useAuth from "./../../../hooks/useAuth";
+import useAuth from "./../../../hooks/useAuth";
 
 // data
 import logoPrimary from "./../../../assets/websiteLogo/logo-primary.webp";
 import { navOptions } from "../../../uiData/navigationOptions";
+
 //cart icon
 import { FiShoppingCart } from "react-icons/fi";
+
+// redux
 import { useSelector } from "react-redux";
 
 const Header = ({ modifyClasses = "" }) => {
-  const cartBooks=useSelector(state=> state.cart)
-  // extra user from auth
-  // const { profileData, appLoading } = useAuth();
+  const cartBooks = useSelector((state) => state.cart);
+  const { profileData } = useAuth();
 
   return (
     <header className={`pt-4 pb-elementGapMd ${modifyClasses}`}>
       <InnerContainer>
         <div className="flex justify-end mb-4 gap-4">
-          <Link className="hover:text-black hover:underline" to={"/login"}>
-            Login
-          </Link>
-          |
-          <Link
-            className="hover:text-black hover:underline"
-            to={"/registration"}
-          >
-            Register Account
-          </Link>
+          {!profileData ? (
+            <>
+              {" "}
+              <Link className="hover:text-black hover:underline" to={"/login"}>
+                Login
+              </Link>
+              |
+              <Link
+                className="hover:text-black hover:underline"
+                to={"/registration"}
+              >
+                Register Account
+              </Link>
+            </>
+          ) : (
+            <p>
+              Logged in as{" "}
+              <Link
+                className="underline hover:text-black"
+                to={profileData.role === "admin" ? "/admin/home" : "/user/home"}
+              >
+                {profileData.name}
+              </Link>
+            </p>
+          )}
           |
           <Link className="hover:text-black hover:underline flex" to={"/cart"}>
             <FiShoppingCart />
             {cartBooks.length}
           </Link>
         </div>
+
         <div className="grid grid-cols-1 gap-elementGapMd sm:gap-0 sm:grid-cols-2 items-center">
           {/* website logo */}
           <div className="justify-self-center sm:justify-self-start flex items-center gap-12">
@@ -60,29 +78,7 @@ const Header = ({ modifyClasses = "" }) => {
             <LargeScreenNav navOptions={navOptions} />
           </div>
 
-          {/* auth related options login/logout etc */}
-          <div className="justify-self-end relative">
-            {/* loading Spinner */}
-            {/* {appLoading && (
-              <LoadingSpinner
-                text="Checking User"
-                modifyClasses="text-2xl pr-4 text-primary"
-                modifyInnerContainerClasses="!flex-row"
-              />
-            )}
-
-            {!appLoading && !profileData && (
-              <LinkBtn text="Sign In" url="/login" />
-            )} */}
-
-            {/* if app is finished loading and user is truthy, show the userprofile */}
-            {/* {!appLoading && profileData && (
-              <LinkBtn text="Go to Dashboard" url="#" />
-            )} */}
-
-            <Searchbox placeholder="Search for books by title" />
-            {/* <div className="absolute w-[25rem] h-[25rem] bg-pink-300 z-10"></div> */}
-          </div>
+          <Searchbox placeholder="Search for books by title" />
         </div>
       </InnerContainer>
     </header>
