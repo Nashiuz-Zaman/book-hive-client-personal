54eea4e0430e67804567b4ab0998ebdaf6aae304
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-02-07 12:27:23 +0600

    Add admin dashboard navigation options

--- src/uiData/navigationOptions.js
+++ src/uiData/navigationOptions.js
@@ -4,3 +4,10 @@ export const navOptions = [
   { id: 2, text: "Shop", url: "#" },
   { id: 3, text: "Contact", url: "/contact" },
 ];
+
+export const adminDashboardNavOptions = [
+  { id: 0, text: "Home", url: "/admin/home" },
+  { id: 1, text: "Users", url: "/admin/users" },
+  { id: 2, text: "Orders", url: "/admin/orders" },
+  { id: 3, text: "Transactions", url: "/admin/transactions" },
+];
