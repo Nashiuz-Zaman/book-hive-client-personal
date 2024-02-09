cae644031498a1e9a1981258237c45248f324f18
mdmonir-hossain <monirsoftengr@gmail.com>
2024-02-09 10:42:40 +0600

    Shopping cart updated

--- src/components/pages/Cart/Cart.jsx
+++ src/components/pages/Cart/Cart.jsx
@@ -1,14 +1,27 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { remove } from "../../../features/cart/cartSlice";
+import useToast from "../../../hooks/useToast";
 
 const Cart = () => {
-  const bookCart = useSelector((state) => state.cart);
+  const { showToast } = useToast();
+  const bookCart = useSelector((state) => state.cart.addedItems);
   const dispatch = useDispatch();
+  // Function to calculate the total price
+  const calculateTotalPrice = () => {
+    let totalPrice = 0;
+    bookCart.forEach((item) => {
+      totalPrice += item.price;
+    });
+    return totalPrice.toFixed(2);
+  };
   const removeToCart = (id) => {
     //dispatch
     dispatch(remove(id));
+    //delete massage
+    showToast("Book Deleted from shopping cart.", "success");
   };
+  
   const carts = bookCart?.map((cartItems) => (
     <div key={cartItems._id} className=" border p-5 space-y-5  flex  ">
       <div>
@@ -21,7 +34,7 @@ const Cart = () => {
       <div className=" flex flex-row justify-evenly items-centerpx-50 grow">
         <h4 className=" font-semibold">{cartItems?.bookName}</h4>
         <h4 className=" text-xs text-gray-400">{cartItems.author}</h4>
-        <h4 className=" text-xs text-gray-400">{cartItems.author}</h4>
+
         <h4 className=" font-semibold">$ {cartItems?.price}</h4>
       </div>
       <button
@@ -33,12 +46,13 @@ const Cart = () => {
     </div>
   ));
   return (
-    <div>
-      <Link to="/">Home</Link>
-      {carts}
-      <div className="flex justify-end ">
-        <button className="bg-pink-500 border  border-pink-500 hover:bg-textPrimary hover:border-textPrimary text-white  w-max transition-all duration-default rounded-default text-center px-6 py-3 ">
-          Place Order
+    <div className="flex">
+      <div className="flex-1">{carts}</div>
+      <div>
+        <h3>Order Summary</h3>
+        <p>Total Price: ${calculateTotalPrice()}</p>
+        <button className="bg-[#F85454]  hover:bg-textPrimary hover:border-textPrimary text-white  w-max transition-all duration-default rounded-default text-center px-6 py-3 ">
+          Proceed To Checkout
         </button>
       </div>
     </div>
